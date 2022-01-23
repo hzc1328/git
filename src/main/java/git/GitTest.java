587ed2ac235d85fce38384f216948ab49dedd390
hezhecong <940818183@qq.com>
2022-01-23 10:08:29 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author cc
+ * @create 2022-01-23 10:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
